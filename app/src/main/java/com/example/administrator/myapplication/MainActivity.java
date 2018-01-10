becb058115d3660fb258b1ab21589f5f8d8fe9f3
华峰 <35133519+HuafengEly@users.noreply.github.com>
2018-01-10 19:23:37 +0800

    添加项目

---
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.administrator.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.first_layout);  //给当前活动加载一个布局
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"You clicked Button 1",Toast.LENGTH_SHORT).show();
+                //finish(); //销毁活动
+            }
+        });
+    }
+
+    @Override
+    /*
+    让菜单显示出来
+     */
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this,"You clicked Add",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this,"You clicked Remove",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+    return true;
+    }
+}
